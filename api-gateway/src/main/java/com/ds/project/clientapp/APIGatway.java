226763267f04d13dbb6d5b6983f618ba3e0bd6d4
Hansani Umayangana Rathnasekara <hansaniuma@gmail.com>
2021-04-28 11:48:09 +0530

    Second-commit

--- api-gateway/src/main/java/com/ds/project/clientapp/APIGatway.java
+++ api-gateway/src/main/java/com/ds/project/clientapp/APIGatway.java
@@ -3,12 +3,12 @@ package com.ds.project.clientapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableZuulProxy
+//@EnableZuulProxy
 public class APIGatway {
 
 	public static void main(String[] args) {
