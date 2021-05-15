e35814dece071cc7613381ad84e66b82bb2c02a0
ipranasinghe <52536005+ipranasinghe@users.noreply.github.com>
2021-05-15 23:48:27 +0530

    Added CRUD operations for user.

--- userservice/src/main/java/com/miniproject/userservice/entity/User.java
+++ userservice/src/main/java/com/miniproject/userservice/entity/User.java
@@ -1,4 +1,24 @@
 package com.miniproject.userservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String userName;
+    private String password;
+    private Role role;
 }
---
+++ userservice/src/main/java/com/miniproject/userservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.miniproject.userservice.repository;
+
+import com.miniproject.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByUserNameContaining(String name);
+}
